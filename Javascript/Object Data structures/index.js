501a32991cdbee33e24d85149a49a0dd8cdb80ba
Ahmad Butt <39210745+AhmadButt00@users.noreply.github.com>
2020-01-20 16:10:24 +0500

    Create index.js
    object data structure exercise

---
+++ Javascript/Object Data structures/index.js
@@ -0,0 +1,19 @@
+var obj = {
+    username: "Ahmad",
+    password: "secret",
+}
+var array = [obj]
+var newsFeed = [
+    {
+        userName: "Ahmad",
+        timeLine: "Hello"
+    },
+    {
+        userName: "Ali",
+        timeLine: "Hi"
+    },
+    {
+        userName: "Asad",
+        timeLine: "chao"
+    },
+]
